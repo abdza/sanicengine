013a0c1d63db07685bf5630ba197f4eb9c8d9eea
abdza <abdullah.zainul@gmail.com>
2018-02-01 13:09:27 +0800

    Now using generic templates

--- pages/forms.py
+++ pages/forms.py
@@ -1,1 +1,11 @@
-from wtforms import Form, StringField
+from wtforms import Form, StringField, TextAreaField, BooleanField, DateField
+
+class PageForm(Form):
+    title = StringField('Title')
+    slug = StringField('Slug')
+    module = StringField('Module')
+    content = TextAreaField('Content')
+    published = BooleanField('Published')
+    require_login = BooleanField('Require Login')
+    publish_date = DateField('Publish Date')
+    expire_date = DateField('Expire Date')
--- pages/views.py
+++ pages/views.py
@@ -2,7 +2,9 @@
 from sanic import Blueprint
 from sanic.response import html, redirect
 from .models import Page
+from .forms import PageForm
 from database import dbsession
+from template import render
 
 bp = Blueprint('pages')
 
@@ -14,3 +16,23 @@ def view(request, slug):
     else:
         print("No page to view")
         return redirect('/')
+
+@bp.route('/pages/create',methods=['POST','GET'])
+@bp.route('/pages/edit/<slug>',methods=['POST','GET'])
+def form(request,slug=None):
+    title = 'Create Page'
+    form = PageForm(request.form)
+    if request.method=='POST' and form.validate():
+        page = dbsession.query(Page).filter_by(slug=form.slug.data).first()
+        if not page:
+            page=Page()
+        form.populate_obj(page)
+        dbsession.add(page)
+        dbsession.commit()
+    else:
+        if slug is not None:
+            page = dbsession.query(Page).filter_by(slug=slug).first()
+            if page:
+                form = PageForm(obj=page)
+                title = 'Edit Page'
+    return html(render('generic/form.html',title=title,form=form))
