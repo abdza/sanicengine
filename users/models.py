85535a772bc7f38325c303fecead47caff4eedc5
abdza <abdullah.zainul@gmail.com>
2018-03-11 22:18:13 +0800

    Now able to edit user in user role

--- users/models.py
+++ users/models.py
@@ -1,5 +1,6 @@
-from database import ModelBase, dbsession
+from database import ModelBase, dbsession, reference_col
 from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy.orm import relationship, backref
 
 class User(ModelBase):
     __tablename__ = 'users'
@@ -7,3 +8,13 @@ class User(ModelBase):
     name = Column(String(250), nullable=False)
     username = Column(String(50))
     password = Column(String(300))
+
+class ModuleRole(ModelBase):
+    __tablename__ = 'module_roles'
+    id = Column(Integer, primary_key=True)
+
+    user_id = reference_col('users')
+    user = relationship('User',backref='roles')
+
+    role = Column(String(100))
+    module = Column(String(200))
