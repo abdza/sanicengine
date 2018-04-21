e27136eeb7f46afcc2e29fdb79e59a8b7d4d72e3
abdza <abdullah.zainul@gmail.com>
2018-04-22 02:22:48 +0800

    Added ability to search in tracker lists

--- trackers/views.py
+++ trackers/views.py
@@ -478,6 +478,8 @@ async def form(request,id=None):
             form.populate_obj(tracker)
             if form['list_fields'].data:
                 tracker.list_fields = ','.join([ ddat.name for ddat in dbsession.execute("select name from tracker_fields where id in (" + form['list_fields'].data + ")") ])
+            if form['search_fields'].data:
+                tracker.search_fields = ','.join([ ddat.name for ddat in dbsession.execute("select name from tracker_fields where id in (" + form['search_fields'].data + ")") ])
             dbsession.add(tracker)
             if newtracker:
                 newstatus = TrackerStatus(name='New',tracker=tracker)
@@ -505,6 +507,10 @@ async def form(request,id=None):
                         'list_fields': {
                             'url': request.app.url_for('trackers.trackerfieldsjson',slug=tracker.slug),
                             'prePopulate':[ {'id':field.id,'name':field.name} for field in tracker.list_fields_list() ]
+                            },
+                        'search_fields': {
+                            'url': request.app.url_for('trackers.trackerfieldsjson',slug=tracker.slug),
+                            'prePopulate':[ {'id':field.id,'name':field.name} for field in tracker.fields_from_list(tracker.search_fields) ]
                             },
                         }
 
