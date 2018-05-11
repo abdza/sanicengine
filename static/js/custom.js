2ecda9d263a7bffdcfa3a6ab25be75b98f4ffd53
abdza <abdullah.zainul@gmail.com>
2018-05-12 02:26:32 +0800

    Separated javascript and css for forms into a separate base

--- static/js/custom.js
+++ static/js/custom.js
@@ -1,40 +1,6 @@
-	// sidebar menu icon
-	$('.menu-icon').on('click', function(){
-		$(this).toggleClass('open');
-		$('.left-side-bar').toggleClass('open');
-	});
+// sidebar menu icon
+$('.menu-icon').on('click', function(){
+	$(this).toggleClass('open');
+	$('.left-side-bar').toggleClass('open');
+});
 
-	// date picker
-	$('.date-picker').datepicker({
-		language: 'en',
-		autoClose: true,
-		dateFormat: 'yyyy-mm-dd',
-	});
-	$('.datetimepicker').datepicker({
-		timepicker: true,
-		language: 'en',
-		autoClose: true,
-		dateFormat: 'yyyy-mm-dd',
-	});
-	$('.datetimepicker-range').datepicker({
-		language: 'en',
-		range: true,
-		multipleDates: true,
-		multipleDatesSeparator: " - "
-	});
-	$('.month-picker').datepicker({
-		language: 'en',
-		minView: 'months',
-		view: 'months',
-		autoClose: true,
-		dateFormat: 'yyyy-mm',
-	});
-
-	// only time picker
-	$( ".time-picker" ).timeDropper({
-		mousewheel: true,
-		meridians: true,
-		init_animation: 'dropdown',
-		setCurrentTime: false
-	});
-	$('.time-picker-default').timeDropper();
