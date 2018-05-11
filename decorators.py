fb23a82e2126aff9a2a5409cda50d0575bca18fe
abdza <abdullah.zainul@gmail.com>
2018-05-11 09:04:46 +0800

    Superuser can access everything. Fixed up node user list

--- decorators.py
+++ decorators.py
@@ -13,33 +13,34 @@ def authorized(object_type=None,require_superuser=False):
             if 'user_id' in request['session']:
                 from users.models import User
                 curuser = dbsession.query(User).filter(User.id==request['session']['user_id']).first()
-            if object_type in ['page','filelink','tracker']:
-                if 'slug' in kwargs:
-                    curobj = None
-                    if object_type=='page':
-                        from pages.models import Page
-                        curobj = dbsession.query(Page).filter(Page.slug==kwargs['slug']).first()
-                    elif object_type=='filelink':
-                        from fileLinks.models import FileLink
-                        curobj = dbsession.query(FileLink).filter(FileLink.slug==kwargs['slug']).first()
-                    elif object_type=='tracker':
-                        from trackers.models import Tracker
-                        curobj = dbsession.query(Tracker).filter(Tracker.slug==kwargs['slug']).first()
-                    if curobj:
-                        if not curobj.require_login:
-                            is_authorized = True
-                        else:
-                            if curuser:
-                                mroles = curuser.moduleroles(curobj.module)
-                                if any(r in ['admin','Admin'] for r in mroles):
-                                    is_authorized = True
-                                if curobj.allowed_roles and len(curobj.allowed_roles) and any(r in [ ar.strip() for ar in curobj.allowed_roles.split(',')] for r in mroles):
-                                    is_authorized = True
+            if object_type in ['page','filelink','tracker'] and 'slug' in kwargs:
+                curobj = None
+                if object_type=='page':
+                    from pages.models import Page
+                    curobj = dbsession.query(Page).filter(Page.slug==kwargs['slug']).first()
+                elif object_type=='filelink':
+                    from fileLinks.models import FileLink
+                    curobj = dbsession.query(FileLink).filter(FileLink.slug==kwargs['slug']).first()
+                elif object_type=='tracker':
+                    from trackers.models import Tracker
+                    curobj = dbsession.query(Tracker).filter(Tracker.slug==kwargs['slug']).first()
+                if curobj:
+                    if not curobj.require_login:
+                        is_authorized = True
+                    else:
+                        if curuser:
+                            mroles = curuser.moduleroles(curobj.module)
+                            if any(r in ['admin','Admin'] for r in mroles):
+                                is_authorized = True
+                            if curobj.allowed_roles and len(curobj.allowed_roles) and any(r in [ ar.strip() for ar in curobj.allowed_roles.split(',')] for r in mroles):
+                                is_authorized = True
             else:
                 if curuser:
                     is_authorized = True
             if curuser and require_superuser:
                 is_authorized = bool(curuser.superuser)
+            if curuser and not is_authorized and bool(curuser.superuser):
+                is_authorized = True
             if is_authorized:
                 # the user is authorized.
                 # run the handler method and return the response
