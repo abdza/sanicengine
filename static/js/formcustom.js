9e373e11f5a06d7aa5f0f215dba8d43b6eec0096
abdza <abdullah.zainul@gmail.com>
2018-05-12 02:26:32 +0800

    Separated javascript and css for forms into a separate base

---
+++ static/js/formcustom.js
@@ -0,0 +1,35 @@
+// date picker
+$('.date-picker').datepicker({
+	language: 'en',
+	autoClose: true,
+	dateFormat: 'yyyy-mm-dd',
+});
+$('.datetimepicker').datepicker({
+	timepicker: true,
+	language: 'en',
+	autoClose: true,
+	dateFormat: 'yyyy-mm-dd',
+	timeFormat: 'hh:ii'
+});
+$('.datetimepicker-range').datepicker({
+	language: 'en',
+	range: true,
+	multipleDates: true,
+	multipleDatesSeparator: " - "
+});
+$('.month-picker').datepicker({
+	language: 'en',
+	minView: 'months',
+	view: 'months',
+	autoClose: true,
+	dateFormat: 'yyyy-mm',
+});
+
+// only time picker
+$( ".time-picker" ).timeDropper({
+	mousewheel: true,
+	meridians: true,
+	init_animation: 'dropdown',
+	setCurrentTime: false
+});
+$('.time-picker-default').timeDropper();
