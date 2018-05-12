c5dfdf196730532a8e8f3e361c1449988feeaaa8
abdza <abdullah.zainul@gmail.com>
2018-05-12 13:59:13 +0800

    Now able to edit the data value in the node

--- trees/forms.py
+++ trees/forms.py
@@ -1,4 +1,5 @@
 from wtforms import Form, StringField, TextAreaField, BooleanField, DateField
+from wtforms.validators import Optional
 
 class TreeForm(Form):
     title = StringField('Title')
@@ -7,19 +8,20 @@ class TreeForm(Form):
     published = BooleanField('Published')
     require_login = BooleanField('Require Login')
     allowed_roles = StringField('Allowed Roles')
-    publish_date = DateField('Publish Date')
-    expire_date = DateField('Expire Date')
+    publish_date = DateField('Publish Date',validators=(Optional(),))
+    expire_date = DateField('Expire Date',validators=(Optional(),))
 
 class TreeNodeForm(Form):
     title = StringField('Title')
     slug = StringField('Slug')
     node_category = StringField('Category')
     node_type = StringField('Type')
+    datastr = TextAreaField('Data')
     published = BooleanField('Published')
     require_login = BooleanField('Require Login')
     allowed_roles = StringField('Allowed Roles')
-    publish_date = DateField('Publish Date')
-    expire_date = DateField('Expire Date')
+    publish_date = DateField('Publish Date',validators=(Optional(),))
+    expire_date = DateField('Expire Date',validators=(Optional(),))
 
 class TreeNodeUserForm(Form):
     user = StringField('User')
