7180236c21928aac2ea1331e4624b8a9d3307dc7
abdza <abdullah.zainul@gmail.com>
2018-05-19 07:42:16 +0800

    Changed location of database settings

--- database.py
+++ database.py
@@ -1,9 +1,14 @@
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship, sessionmaker
 from sqlalchemy import create_engine, Column, ForeignKey
+try:
+    import settings
+except ImportError:
+    print "Error importing settings. Please copy settings.sample.py to settings.py and modify the content for your use"
+    exit()
 
 ModelBase = declarative_base()
-dbengine = create_engine('postgresql://sanic:sanic123@localhost/sanictest')
+dbengine = create_engine('postgresql://' + settings.DATABASE_USERNAME + ':' + settings.DATABASE_PASSWORD + '@localhost/' + settings.DATABASE_NAME)
 dbsessionmaker = sessionmaker(bind=dbengine)
 dbsession = dbsessionmaker()
 
