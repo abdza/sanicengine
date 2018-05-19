425d3e0998f2e2a7865313dbe37a5a29f980565b
abdza <abdullah.zainul@gmail.com>
2018-05-19 10:18:09 +0800

    Fixed sql scripts

--- setupdb.py
+++ setupdb.py
@@ -6,8 +6,7 @@ except ImportError:
     print("Error importing settings. Please copy settings.sample.py to settings.py and modify the content for your use")
     exit()
 
-print("create database " + settings.DATABASE_NAME)
-print("create role " + settings.DATABASE_USERNAME + " with password " + settings.DATABASE_PASSWORD)
-print("grant all on database " + settings.DATABASE_NAME + " to " + settings.DATABASE_USERNAME)
-print("flush privileges")
+print("create database " + settings.DATABASE_NAME + ";")
+print("create user " + settings.DATABASE_USERNAME + " with password '" + settings.DATABASE_PASSWORD + "';")
+print("grant all on database " + settings.DATABASE_NAME + " to " + settings.DATABASE_USERNAME + ";")
 
