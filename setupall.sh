f2b59935bd68087aa8eb0d2589a364a1d7dfd7d4
abdza <abdullah.zainul@gmail.com>
2019-01-02 00:42:46 +0800

    Added sudo to chown in setupall script

--- setupall.sh
+++ setupall.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 ./dbscripts.py | sudo -u postgres psql
 mkdir custom_module
-chown -R www-data custom_module
+sudo chown -R www-data custom_module
 mkdir uploads
-chown -R www-data uploads
+sudo chown -R www-data uploads
